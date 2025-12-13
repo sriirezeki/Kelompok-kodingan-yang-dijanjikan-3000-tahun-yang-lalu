b75736891b6cc2b11144d179715023149dc7cde2
Ibnu <ibnuadha123@gmail.com>
2025-12-14 03:38:20 +0700

    feat: add default menu and table initialization and add additional comment for each procedure and each menu category

--- System_POS_Restaurant.pas
+++ System_POS_Restaurant.pas
@@ -68,6 +68,7 @@ begin
   end;
 end;
 
+// format rupiah
 function FormatRupiah(nilai: longint): string;
 var
   s: string;
@@ -84,3 +85,71 @@ begin
   FormatRupiah := 'Rp ' + s;
 end;
 
+// inisialisasi data menu default
+procedure InitMenu;
+begin
+  jumlahMenu := 15;
+  
+  // Makanan
+  menu[1].kode := 'M001'; menu[1].nama := 'Nasi Goreng Special'; menu[1].kategori := Makanan; 
+  menu[1].harga := 25000; menu[1].stok := 50; menu[1].tersedia := true;
+  
+  menu[2].kode := 'M002'; menu[2].nama := 'Mie Goreng'; menu[2].kategori := Makanan;
+  menu[2].harga := 20000; menu[2].stok := 50; menu[2].tersedia := true;
+  
+  menu[3].kode := 'M003'; menu[3].nama := 'Ayam Bakar'; menu[3].kategori := Makanan;
+  menu[3].harga := 30000; menu[3].stok := 30; menu[3].tersedia := true;
+  
+  menu[4].kode := 'M004'; menu[4].nama := 'Sate Ayam'; menu[4].kategori := Makanan;
+  menu[4].harga := 28000; menu[4].stok := 40; menu[4].tersedia := true;
+  
+  menu[5].kode := 'M005'; menu[5].nama := 'Gado-Gado'; menu[5].kategori := Makanan;
+  menu[5].harga := 18000; menu[5].stok := 35; menu[5].tersedia := true;
+  
+  // Minuman
+  menu[6].kode := 'D001'; menu[6].nama := 'Es Teh Manis'; menu[6].kategori := Minuman;
+  menu[6].harga := 5000; menu[6].stok := 100; menu[6].tersedia := true;
+  
+  menu[7].kode := 'D002'; menu[7].nama := 'Es Jeruk'; menu[7].kategori := Minuman;
+  menu[7].harga := 8000; menu[7].stok := 80; menu[7].tersedia := true;
+  
+  menu[8].kode := 'D003'; menu[8].nama := 'Cappuccino'; menu[8].kategori := Minuman;
+  menu[8].harga := 15000; menu[8].stok := 60; menu[8].tersedia := true;
+  
+  menu[9].kode := 'D004'; menu[9].nama := 'Jus Alpukat'; menu[9].kategori := Minuman;
+  menu[9].harga := 12000; menu[9].stok := 50; menu[9].tersedia := true;
+  
+  menu[10].kode := 'D005'; menu[10].nama := 'Thai Tea'; menu[10].kategori := Minuman;
+  menu[10].harga := 10000; menu[10].stok := 70; menu[10].tersedia := true;
+  
+  // Dessert
+  menu[11].kode := 'DS001'; menu[11].nama := 'Ice Cream Vanilla'; menu[11].kategori := Dessert;
+  menu[11].harga := 15000; menu[11].stok := 40; menu[11].tersedia := true;
+  
+  menu[12].kode := 'DS002'; menu[12].nama := 'Puding Coklat'; menu[12].kategori := Dessert;
+  menu[12].harga := 12000; menu[12].stok := 35; menu[12].tersedia := true;
+  
+  // Snack
+  menu[13].kode := 'S001'; menu[13].nama := 'Kentang Goreng'; menu[13].kategori := Snack;
+  menu[13].harga := 15000; menu[13].stok := 50; menu[13].tersedia := true;
+  
+  menu[14].kode := 'S002'; menu[14].nama := 'Risoles Mayo'; menu[14].kategori := Snack;
+  menu[14].harga := 10000; menu[14].stok := 45; menu[14].tersedia := true;
+  
+  menu[15].kode := 'S003'; menu[15].nama := 'Onion Rings'; menu[15].kategori := Snack;
+  menu[15].harga := 18000; menu[15].stok := 40; menu[15].tersedia := true;
+end;
+
+// inisialisasi meja
+procedure InitMeja;
+var i: integer;
+begin
+  for i := 1 to MAX_MEJA do
+  begin
+    meja[i].nomor := i;
+    meja[i].terisi := false;
+    meja[i].namaCustomer := '';
+    meja[i].jumlahItem := 0;
+    meja[i].totalBayar := 0;
+  end;
+end;
